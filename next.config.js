ab388f94af7d86bae2b56179f1a49a13b9fdadb2
Dhaval Tank <dhavaltank2001@gmail.com>
2024-02-25 14:11:20 +0530

    update workflow file

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: "export",
   // poweredByHeader: false,
   // trailingSlash: true,
-  // basePath: '/dhavaltank',
-  // assetPrefix: '/dhavaltank/',
+  basePath: '/dhavaltank',
+  assetPrefix: '/dhavaltank/',
   reactStrictMode: true,
 };
